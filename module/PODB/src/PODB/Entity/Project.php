a310163ebc19f22741c67e308c34ccbdcf918215
cwreden <christian.wreden@googlemail.com>
2013-10-29 09:27:57 +0100

    Project Entity basic structure

---
+++ module/PODB/src/PODB/Entity/Project.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PODB\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Project
+ * @package PODB\Entity
+ * @ORM\Entity
+ */
+class Project {
+
+    /**
+     * @var
+     * @ORM\ID
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @var
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    protected $default_language;
+
+    protected $createdBy;
+
+    protected $createDate;
+
+    protected $lastUpdateBy;
+
+    protected $lastUpdateDate;
+} 
