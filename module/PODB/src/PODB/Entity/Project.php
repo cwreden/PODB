6b33f575ebd25ee04174218215ef81a50998f774
cwreden <christian.wreden@googlemail.com>
2013-11-16 00:00:46 +0100

    Getter und Setter sowie asArray Funktion implementiert.

--- module/PODB/src/PODB/Entity/Project.php
+++ module/PODB/src/PODB/Entity/Project.php
@@ -53,4 +53,142 @@ class Project extends BaseEntity
     protected $lastUpdateBy;
 
     protected $lastUpdateDate;
+
+    /**
+     * @param mixed $createDate
+     */
+    public function setCreateDate($createDate)
+    {
+        $this->createDate = $createDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreateDate()
+    {
+        return $this->createDate;
+    }
+
+    /**
+     * @param mixed $createdBy
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param mixed $default_language
+     */
+    public function setDefaultLanguage($default_language)
+    {
+        $this->default_language = $default_language;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDefaultLanguage()
+    {
+        return $this->default_language;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $lastUpdateBy
+     */
+    public function setLastUpdateBy($lastUpdateBy)
+    {
+        $this->lastUpdateBy = $lastUpdateBy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastUpdateBy()
+    {
+        return $this->lastUpdateBy;
+    }
+
+    /**
+     * @param mixed $lastUpdateDate
+     */
+    public function setLastUpdateDate($lastUpdateDate)
+    {
+        $this->lastUpdateDate = $lastUpdateDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastUpdateDate()
+    {
+        return $this->lastUpdateDate;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $users
+     */
+    public function setUsers($users)
+    {
+        $this->users = $users;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    public function asArray()
+    {
+        return array(
+            'id' => $this->getId(),
+            'name' => $this->getName(),
+            'defaultLanguage' => $this->getDefaultLanguage(),
+            'users' => $this->getUsers()
+        );
+    }
 } 
