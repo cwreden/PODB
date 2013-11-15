de4f3ec1524eecde2cb4cccbb99789aad589bb03
cwreden <christian.wreden@googlemail.com>
2013-11-15 23:52:25 +0100

    Controller REST Funktionen implementiert.

--- module/PODB/src/PODB/Controller/PODomainController.php
+++ module/PODB/src/PODB/Controller/PODomainController.php
@@ -2,9 +2,68 @@
 
 namespace PODB\Controller;
 
-use \Zend\Mvc\Controller\AbstractRestfulController;
+use Exception;
+use PODB\Entity\PODomain;
+use PODB\Repository\PODomainRepository;
+use Zend\View\Model\JsonModel;
 
-class PODomainController extends AbstractRestfulController
+class PODomainController extends BaseRestfulController
 {
+    public function getList()
+    {
+        $objects = $this->getRepository()->getAll();
 
+        $output = array();
+        foreach ($objects as $object) {
+            $output[] = $object->asArray();
+        }
+
+        return new JsonModel($output);
+    }
+
+    public function get($id)
+    {
+        return new JsonModel($this->getRepository()->get($id)->asArray());
+    }
+
+    public function create($data)
+    {
+        $object = new PODomain();
+        $object->setName($data['name']);
+        $object->setProjectId($data['projectId']);
+        $object->setCreateDate(time());
+        return new JsonModel(array('id' => $this->getRepository()->create($object)));
+    }
+
+    public function update($id, $data)
+    {
+        try {
+            $object = $this->getRepository()->get($id);
+            $object->setName($data['name']);
+            $object->setProjectId($data['projectId']);
+            $object->setLastUpdateDate(time());
+            $this->getRepository()->update($object);
+            return new JsonModel(array('successfull' => 'true'));
+        } catch (Exception $e) {
+            return $this->getErrorModel($e);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $this->getRepository()->delete($id);
+            return new JsonModel(array('successful' => 'true'));
+        } catch (Exception $e) {
+            return $this->getErrorModel($e);
+        }
+    }
+
+    /**
+     * @return PODomainRepository
+     */
+    protected function getRepository()
+    {
+        return $this->getServiceLocator()->get('PODB\Repository\PODomain');
+    }
 }
