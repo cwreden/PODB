97230b94b72db7d0c66503299adc06c7ccb1490a
cwreden <christian.wreden@googlemail.com>
2013-11-15 23:56:56 +0100

    Getter und Setter sowie asArray Funktion implementiert.

--- module/PODB/src/PODB/Entity/PODataset.php
+++ module/PODB/src/PODB/Entity/PODataset.php
@@ -47,4 +47,125 @@ class PODataSet extends BaseEntity
     protected $lastUpdateBy;
 
     protected $lastUpdateDate;
+
+    /**
+     * @param mixed $createDate
+     */
+    public function setCreateDate($createDate)
+    {
+        $this->createDate = $createDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreateDate()
+    {
+        return $this->createDate;
+    }
+
+    /**
+     * @param mixed $createdBy
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param mixed $domainId
+     */
+    public function setDomainId($domainId)
+    {
+        $this->domainId = $domainId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDomainId()
+    {
+        return $this->domainId;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $lastUpdateBy
+     */
+    public function setLastUpdateBy($lastUpdateBy)
+    {
+        $this->lastUpdateBy = $lastUpdateBy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastUpdateBy()
+    {
+        return $this->lastUpdateBy;
+    }
+
+    /**
+     * @param mixed $lastUpdateDate
+     */
+    public function setLastUpdateDate($lastUpdateDate)
+    {
+        $this->lastUpdateDate = $lastUpdateDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastUpdateDate()
+    {
+        return $this->lastUpdateDate;
+    }
+
+    /**
+     * @param mixed $msgId
+     */
+    public function setMsgId($msgId)
+    {
+        $this->msgId = $msgId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMsgId()
+    {
+        return $this->msgId;
+    }
+
+    public function asArray()
+    {
+        return array(
+            'id' => $this->getId(),
+            'domainId' => $this->getDomainId(),
+            'msgId' => $this->getMsgId()
+        );
+    }
 } 
