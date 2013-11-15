c6e4382a7546ecc0cb98150381a82440285a6e18
cwreden <christian.wreden@googlemail.com>
2013-11-15 23:51:54 +0100

    Getter und Setter hinzugefügt; asArray-Funktion implementiert.

--- module/PODB/src/PODB/Entity/PODomain.php
+++ module/PODB/src/PODB/Entity/PODomain.php
@@ -47,4 +47,124 @@ class PODomain extends BaseEntity
     protected $lastUpdateBy;
 
     protected $lastUpdateDate;
+
+    /**
+     * @param mixed $createDate
+     */
+    public function setCreateDate($createDate)
+    {
+        $this->createDate = $createDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreateDate()
+    {
+        return $this->createDate;
+    }
+
+    /**
+     * @param mixed $createdBy
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $lastUpdateBy
+     */
+    public function setLastUpdateBy($lastUpdateBy)
+    {
+        $this->lastUpdateBy = $lastUpdateBy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastUpdateBy()
+    {
+        return $this->lastUpdateBy;
+    }
+
+    /**
+     * @param mixed $lastUpdateDate
+     */
+    public function setLastUpdateDate($lastUpdateDate)
+    {
+        $this->lastUpdateDate = $lastUpdateDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastUpdateDate()
+    {
+        return $this->lastUpdateDate;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $projectId
+     */
+    public function setProjectId($projectId)
+    {
+        $this->projectId = $projectId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProjectId()
+    {
+        return $this->projectId;
+    }
+
+    public function asArray()
+    {
+        return array(
+            'id' => $this->getId(),
+            'name' => $this->getName()
+        );
+    }
 } 
