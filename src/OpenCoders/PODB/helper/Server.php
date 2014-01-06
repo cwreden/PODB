86d17d80571e0f902096ae5c6ffde34c319c35a8
André Meyerjürgens <ameyerjuergens@gmail.com>
2014-01-07 00:30:02 +0100

    implemented server helper to resolve ipadress / http protocol from server

---
+++ src/OpenCoders/PODB/helper/Server.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OpenCoders\PODB\helper;
+
+
+class Server {
+
+    static public function getBaseApiUrl()
+    {
+        return self::getHostAdress() . "/api";
+    }
+
+    static public function getHostAdress()
+    {
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        return $protocol . $_SERVER['HTTP_HOST'];
+    }
+
+} 
