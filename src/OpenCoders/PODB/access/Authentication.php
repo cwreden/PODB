7ce65725999245c09732bc1d5b00d220ab41f0be
cwreden <christian.wreden@googlemail.com>
2014-01-06 23:46:30 +0100

    isAllowed added

--- src/OpenCoders/PODB/access/Authentication.php
+++ src/OpenCoders/PODB/access/Authentication.php
@@ -1,14 +1,23 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: cwreden
- * Date: 06.01.14
- * Time: 18:30
- */
 
 namespace OpenCoders\PODB\access;
 
 
 class Authentication {
 
+    /**
+     * Access verification method.
+     *
+     * API access will be denied when this method returns false
+     *
+     * @return boolean true when api access is allowed false otherwise
+     */
+    public function __isAllowed()
+    {
+//        var_dump($this->getNeededPermissions());
+
+        // TODO Logic
+
+        return true;
+    }
 } 
