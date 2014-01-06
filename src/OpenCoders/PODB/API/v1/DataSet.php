fc9366eac6cf29b3a4a200c56f9367757d644d78
cwreden <christian.wreden@googlemail.com>
2014-01-06 21:29:11 +0100

    Base DataSet API created.

---
+++ src/OpenCoders/PODB/API/v1/DataSet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OpenCoders\PODB\API\v1;
+
+
+class DataSet {
+
+    /**
+     * @url GET /dataSets/:id
+     */
+    public function get($id)
+    {
+        return array();
+    }
+
+    /**
+     * @URL GET /dataSets/:id/translations
+     */
+    public function getTranslations()
+    {
+
+    }
+} 
