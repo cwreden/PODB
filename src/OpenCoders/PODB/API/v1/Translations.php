660ce42e9822282b952ef14d2f76fe8541f9bc35
André Meyerjürgens <ameyerjuergens@gmail.com>
2014-01-06 21:12:28 +0100

    scaffold REST API for translations

---
+++ src/OpenCoders/PODB/API/v1/Translations.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace OpenCoders\PODB\API\v1;
+
+class Translations
+{
+
+    /**
+     * @url /translations
+     */
+    public function getList()
+    {
+
+    }
+
+    /**
+     * @param $id
+     * @url /translations/:id
+     */
+    public function get($id)
+    {
+
+    }
+
+}
