2ab680b1b28878bb313dde6e033aa972762db41c
cwreden <christian.wreden@googlemail.com>
2014-01-06 17:14:27 +0100

    Autoloader und API-Index hinzugefügt.

---
+++ public/api/index.php
@@ -0,0 +1,15 @@
+<?php
+
+set_include_path('.'); // Remove default include_path so only using Composer include_path
+
+require __DIR__ . '/../../src/autoload.php';
+
+\Luracast\Restler\Defaults::$useUrlBasedVersioning = true;
+
+$restler = new \Luracast\Restler\Restler();
+$restler->setAPIVersion(1);
+//$restler->addAuthenticationClass('OpenCoders\PODB\Auth');
+
+//$restler->addAPIClass('RestTEST\Under\FirstUnderClass', 'lala');
+
+$restler->handle();
