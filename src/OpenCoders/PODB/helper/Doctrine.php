7d689795fb45a2c47913917f6d8f023c1c434494
André Meyerjürgens <ameyerjuergens@gmail.com>
2014-01-07 16:08:21 +0100

    documented Doctrine Helper Class (and a little refactoring)

--- src/OpenCoders/PODB/helper/Doctrine.php
+++ src/OpenCoders/PODB/helper/Doctrine.php
@@ -7,28 +7,33 @@ use Doctrine\Common\Cache\ArrayCache;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 
-class Doctrine {
+class Doctrine
+{
 
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
     static private $em = null;
 
+    /**
+     * configuration parameter for EntityManager instanciation (if you want to run doctrine in development mode)
+     * @var bool
+     */
     static private $isDevMode = false;
 
+    /**
+     * Returns a singleton instance of the Doctrine EntityManager
+     *
+     * @return \Doctrine\ORM\EntityManager
+     */
     static public function getEntityManager()
     {
         if (self::$em == null) {
-
-            $paths = array(__DIR__ . "/../Entity");
-
-            // the connection configuration
             $dbParams = include(__DIR__ . '/../../../../config/doctrine.local.php');
-
+            $paths = array(__DIR__ . "/../Entity");
             $config = Setup::createAnnotationMetadataConfiguration($paths, self::$isDevMode);
 
-            if (self::$isDevMode) {
-                $cache = new ArrayCache();  // @ToDo: How does the Array Cache works?
-            } else {
-                $cache = new ApcCache();    // @ToDo: How does the APC Cache works?
-            }
+            $cache = (self::$isDevMode ? new ArrayCache() : new ApcCache()); // @ToDo: How does the Array Cache and APC Cache works?
             $config->setMetadataCacheImpl($cache);
             $config->setQueryCacheImpl($cache);
 
@@ -38,6 +43,10 @@ class Doctrine {
         return self::$em;
     }
 
+    /**
+     * Set true, if you want to run Doctrine's EntityManager in development mode (call this method before calling getEntityManager)
+     * @param boolean $modeBoolean
+     */
     static public function setDevMode($modeBoolean)
     {
         self::$isDevMode = $modeBoolean;
