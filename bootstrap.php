994ba3817a2c5c922bd57aed202de8d83231b2b5
cwreden <christian.wreden@googlemail.com>
2014-01-07 15:26:53 +0100

    Doctrine configuration

--- bootstrap.php
+++ bootstrap.php
@@ -1,21 +1,16 @@
 <?php
 // bootstrap.php
 // Include Composer Autoload (relative to project root).
-require_once "vendor/autoload.php";
+require_once "src/autoload.php";
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
-$paths = array("/path/to/entity-files");
+$paths = array("src/OpenCoders/PODB/Entity");
 $isDevMode = false;
 
 // the connection configuration
-$dbParams = array(
-    'driver'   => 'pdo_mysql',
-    'user'     => 'root',
-    'password' => '',
-    'dbname'   => 'podb',
-);
+$dbParams = include('config/doctrine.local.php');
 
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
 $entityManager = EntityManager::create($dbParams, $config);
