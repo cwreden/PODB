057869cceec2691e030c8643b83a24cca40f0a55
cwreden <christian.wreden@googlemail.com>
2014-01-07 14:35:52 +0100

    Test RestAngularJS function finished

--- public/js/app/controller/TestController.js
+++ public/js/app/controller/TestController.js
@@ -4,7 +4,7 @@ var testController;
 angular.module('app').controller('TestController', function($scope, Restangular) {
 
     $scope.getAll = function (objectType) {
-        $scope.createdObject = Restangular.all(objectType).getList();
+        $scope.allObjects = Restangular.all(objectType).getList();
     };
     $scope.getObject = function (objectType, identifier) {
         $scope.oneObject = Restangular.one(objectType, identifier).get();
@@ -12,12 +12,18 @@ angular.module('app').controller('TestController', function($scope, Restangular)
     $scope.createObject = function (objectType, data) {
         $scope.createdObject = Restangular.all(objectType).post(data);
     };
-    $scope.updateObject = function (objectType, data) {
-//        $scope.updatedObject = Restangular.all(objectType).put(data);
+    $scope.updateObject = function (objectType, identifier, data) {
+        Restangular.one(objectType, identifier).get().then(function (object) {
+            Object.keys(data).forEach(function (key) {
+                object[key] = data[key];
+            });
+            $scope.updatedObject = object.put();
+        });
     };
     $scope.deleteObject = function (objectType, identifer) {
-//        $scope.deletedObject = Restangular.one(objectType, identifer).get();
-//        $scope.deletedObject.remove();
+        $scope.deletedObject = Restangular.one(objectType, identifer).get().then(function (object) {
+            object.remove();
+        });
     };
 
     testController = $scope;
