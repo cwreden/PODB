67dd9318a328433db191d6ef010037dc12c41806
cwreden <christian.wreden@googlemail.com>
2014-01-08 12:36:10 +0100

    Empty parameter exception added

---
+++ src/OpenCoders/PODB/Exception/EmptyParameterException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OpenCoders\PODB\Exception;
+
+
+use Exception;
+
+class EmptyParameterException extends PodbException {
+
+    public function __construct($message = "Parameter not allowed to be empty.", $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
