6b00e415670cd75ea93d0ee55f6b7d87f844b71a
cwreden <christian.wreden@googlemail.com>
2014-01-12 14:40:23 +0100

    Config manager implemented

---
+++ src/OpenCoders/PODB/config/ConfigManager.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace OpenCoders\PODB\config;
+
+
+class ConfigManager {
+
+    /**
+     * Config folder
+     */
+    private $configPath;
+
+    /**
+     * Rate limit config File
+     */
+    private $rateLimitConfigFile = 'rateLimit.config.php';
+
+    /**
+     * Main Config File
+     */
+    private $mainConfigFile = 'podb.config.php';
+
+    /**
+     *
+     */
+    public function __construct ()
+    {
+        $this->configPath = __DIR__ . '/../../../config/';
+    }
+
+    /**
+     * @return array
+     */
+    public function getRateLimit ()
+    {
+        return include($this->configPath . $this->rateLimitConfigFile);
+    }
+
+    /**
+     * @return array
+     */
+    public function getSessionConfig()
+    {
+        $mainConfig = include($this->configPath . $this->mainConfigFile);
+
+        return $mainConfig['session'];
+    }
+} 
