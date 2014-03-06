d73e4d04f3d110629fcf33e70efb73124053a08f
cwreden <christian.wreden@googlemail.com>
2014-03-06 09:45:00 +0100

    Keep API

---
+++ public/api/index.php
@@ -0,0 +1,29 @@
+<?php
+
+use Luracast\Restler\Defaults;
+use Luracast\Restler\Restler;
+
+set_include_path('.'); // Remove default include_path so only using Composer include_path
+
+require __DIR__ . '/../../src/autoload.php';
+
+Defaults::$useUrlBasedVersioning = true;
+//Defaults::$cacheDirectory = '/../../tmp/restler/cache';
+
+$restler = new Restler();
+$restler->setAPIVersion(1);
+$restler->addAuthenticationClass('OpenCoders\Podb\Access\BasicAuthentication');
+
+$restler->addFilterClass('OpenCoders\Podb\Api\Filter\RateLimit');
+
+$restler->addAPIClass('OpenCoders\Podb\Api\Users', '');
+$restler->addAPIClass('OpenCoders\Podb\Api\Projects', '');
+$restler->addAPIClass('OpenCoders\Podb\Api\Languages', '');
+$restler->addAPIClass('OpenCoders\Podb\Api\Domains', '');
+$restler->addAPIClass('OpenCoders\Podb\Api\DataSets', '');
+$restler->addAPIClass('OpenCoders\Podb\Api\Translations', '');
+$restler->addAPIClass('OpenCoders\Podb\Api\Audit');
+
+$restler->addAPIClass('Resources');
+
+$restler->handle();
