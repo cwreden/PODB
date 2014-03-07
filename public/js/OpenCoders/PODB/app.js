26b9beefe240ebf1b59cd824604927525ca996eb
cwreden <christian.wreden@googlemail.com>
2014-03-07 09:48:26 +0100

    Title update implemented and users route integrated.

--- public/js/OpenCoders/PODB/app.js
+++ public/js/OpenCoders/PODB/app.js
@@ -1,16 +1,18 @@
 'use Strict';
 
-angular.module('PODB', ['restangular', 'ngRoute'])
+var podb = angular.module('PODB', ['restangular', 'ngRoute'])
     .config(['RestangularProvider', '$routeProvider',
         function (RestangularProvider, $routeProvider) {
             RestangularProvider.setBaseUrl('/api/v1');
 
         $routeProvider
             .when('/dashboard', {
+                title: 'Dashboard',
                 templateUrl: '/template/dashboard.html',
                 controller: 'DashboardController'
             })
             .when('/users', {
+                title: 'Users',
                 templateUrl: '/template/users/list.html',
                 controller: 'UserListController'
             })
@@ -23,3 +25,13 @@ angular.module('PODB', ['restangular', 'ngRoute'])
             });
         }
     ]);
+
+podb.run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        if (current != undefined && current.$$route != undefined && current.$$route.title != undefined) {
+            $rootScope.title = current.$$route.title + ' | PO-Database';
+        } else {
+            $rootScope.title = 'PO-Database';
+        }
+    });
+}]);
