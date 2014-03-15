05ee9de5b3750b0d6f9704723ef1ddec52dbe7b3
cwreden <christian.wreden@googlemail.com>
2014-03-15 01:00:24 +0100

    Registration prototype.

---
+++ public/js/OpenCoders/PODB/controller/RegistrationController.js
@@ -0,0 +1,6 @@
+'use Strict';
+
+angular.module('PODB')
+    .controller('RegistrationController', ['$scope', '$http', function($scope, $http) {
+        console.log('registration control');
+    }]);
