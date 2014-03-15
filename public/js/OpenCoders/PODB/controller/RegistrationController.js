d33cd00e9abb0f71ff665d3ad951b8a36623fad4
cwreden <christian.wreden@googlemail.com>
2014-03-15 10:47:32 +0100

    Registration responsive

--- public/js/OpenCoders/PODB/controller/RegistrationController.js
+++ public/js/OpenCoders/PODB/controller/RegistrationController.js
@@ -1,6 +1,23 @@
 'use Strict';
 
+/**
+ * TODO
+ */
 angular.module('PODB')
     .controller('RegistrationController', ['$scope', '$http', function($scope, $http) {
-        console.log('registration control');
+        $scope.processingRegistration = false;
+        $scope.registrationData = {};
+
+        $scope.register = function () {
+            $scope.processingRegistration = true;
+            $http.post('/api/user/register', $scope.registrationData)
+                .success(function (data) {
+                    console.log('reg true', data);
+                    $scope.processingRegistration = false;
+                })
+                .error(function (data, status) {
+                    console.log('reg false', data, status);
+                    $scope.processingRegistration = false;
+                });
+        };
     }]);
