f0aca56f25ef95b47edeebed6df2b80a36d4b362
cwreden <christian.wreden@googlemail.com>
2014-03-23 02:30:24 +0100

    Reset registration fields

--- public/js/OpenCoders/PODB/controller/RegistrationController.js
+++ public/js/OpenCoders/PODB/controller/RegistrationController.js
@@ -7,12 +7,15 @@ angular.module('PODB')
     .controller('RegistrationController', ['$scope', '$http', 'apiBaseUrl', function($scope, $http, apiBaseUrl) {
         $scope.processingRegistration = false;
         $scope.registrationData = {};
+        $scope.success = false;
 
         $scope.register = function () {
             $scope.processingRegistration = true;
             $http.post(apiBaseUrl + '/user/register', $scope.registrationData)
                 .success(function (data) {
                     $scope.processingRegistration = false;
+                    $scope.success = true;
+                    $scope.registrationData = {};
                 })
                 .error(function (data, status) {
                     $scope.processingRegistration = false;
