3fd5a062e03599297e63c0665ad42397185d72f6
cwreden <christian.wreden@googlemail.com>
2014-03-23 20:27:38 +0100

    Profile prototype

--- public/js/OpenCoders/PODB/controller/ProfileController.js
+++ public/js/OpenCoders/PODB/controller/ProfileController.js
@@ -2,5 +2,15 @@
 
 angular.module('PODB')
     .controller('ProfileController', ['$scope', '$http', function($scope, $http) {
+        $scope.user = {
+            'displayName': 'Max Mutter',
+            'email': 'lalalala@lala.lalal',
+            'company': 'The Org',
+            'location': 'Oldenburg',
+            'blog': 'the.web.de',
+            'joinedOn': 12345678,
+            'projectCount': 3,
+            'gravatar': 'http://thetransformedmale.files.wordpress.com/2011/06/bruce-wayne-armani.jpg'
+        };
         console.log('profile control');
     }]);
